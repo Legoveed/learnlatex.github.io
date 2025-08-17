346c4eba7975b0d5adcb535833da1f59af0aa3a5
Legoveed <84409977+Legoveed@users.noreply.github.com>
2025-08-18 00:12:56 +0300

    Update buttons.js

--- ru/includes/buttons.js
+++ ru/includes/buttons.js
@@ -1,13 +1,13 @@
 runlatex.texts ={
-    "Open in Overleaf": "Open in Overleaf",
-    "TeXLive.net":      "Run at TeXLive.net",
-    "Delete Output":    "Delete Output",
-    "Compiling PDF":    "Compiling PDF",
+    "Open in Overleaf": "Открыть в Overleaf",
+    "TeXLive.net":      "Запустить на TeXLive.net",
+    "Delete Output":    "Стереть вывод",
+    "Compiling PDF":    "Компилляция в PDF",
     // The following not used on learnlatex.org
-    "edit":             "edit",
-    "copy":             "copy",
-    "Added Code":       "Added code",
-    "End Added Code":   "End Added code",
-    "Top Caption":      "Edit and run this example:"
+    "edit":             "редактировать",
+    "copy":             "копировать",
+    "Added Code":       "Добавленный код",
+    "End Added Code":   "Конец добавленного кода",
+    "Top Caption":      "Редактировать и запустить этот пример:"
 }
 
